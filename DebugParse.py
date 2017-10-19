da5a700323980678fae0409c6e1aba7da6e70eea
phasedscum <hyrod88@gmail.com>
2017-10-19 12:28:27 +0800

    V0.1

--- DebugParse.py
+++ DebugParse.py
@@ -7,10 +7,10 @@ print('Script v0.1 Beta')
 
 filename = input('Enter full file name with extension : ')
 log_desc = []
-pattern = re.compile("serial", re.IGNORECASE)
+pattern = re.compile("started", re.IGNORECASE)
 try:
     with open(filename, 'rt') as log_name:
-        for linenum, line in enumerate(log_name):
+      for linenum, line in enumerate(log_name):
          if pattern.search(line) != None:
             log_desc.append((linenum, line.rstrip('\n')))
             for linenum, line in log_desc:
