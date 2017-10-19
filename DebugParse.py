058edb5ba6d97a0d038052d8be574b588e746009
phasedscum <hyrod88@gmail.com>
2017-10-19 12:24:15 +0800

    V0.1

---
+++ DebugParse.py
@@ -0,0 +1,19 @@
+from colorama import init
+import re
+# Test Parser to read debug files
+
+print('This is a test script to read and define q931 debug output')
+print('Script v0.1 Beta')
+
+filename = input('Enter full file name with extension : ')
+log_desc = []
+pattern = re.compile("serial", re.IGNORECASE)
+try:
+    with open(filename, 'rt') as log_name:
+        for linenum, line in enumerate(log_name):
+         if pattern.search(line) != None:
+            log_desc.append((linenum, line.rstrip('\n')))
+            for linenum, line in log_desc:
+                print(linenum, ": ", line, sep='')
+except FileNotFoundError:
+    print('Invalid File / File Not Found')
