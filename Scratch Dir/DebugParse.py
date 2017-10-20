d9f6d6f5773baade716ee806450f46b5a7845f15
phasedscum <hyrod88@gmail.com>
2017-10-20 16:57:49 +0800

    License Pattern Definitions

--- Scratch Dir/DebugParse.py
+++ Scratch Dir/DebugParse.py
@@ -3,19 +3,21 @@ import re
 import sys
 # Test Parser to read debug files
 
-print('This is a test script to read and define q931 debug output')
+print('This is a test script to read and define CUC Command output')
 print('Script v0.1 Beta')
 
 filename = input('Enter full file name with extension : ')
 log_desc = []
-pattern = re.compile("started", re.IGNORECASE)
+pattern = re.compile("stopped",  re.IGNORECASE)
+pattern2 = re.compile("DB Version", re.IGNORECASE)
+
 try:
     with open(filename, 'rt') as log_name:
       for linenum, line in enumerate(log_name):
-         if pattern.search(line) != None:
+         if pattern.search(line) is not None:
             log_desc.append((linenum, line.rstrip('\n')))
             #for linenum, line in log_desc:
-            print(linenum, ": ", line, sep='')
+            print("- ", line, sep='')
 except FileNotFoundError:
     print('Invalid File / File Not Found')
 sys.exit(1)
